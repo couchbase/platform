c16eedf93aa3e3bfc633e0157584b789f46fe6f8
Trond Norbye <trond.norbye@gmail.com>
2014-03-25 14:26:22 +0100

    Reorder checks in parse_value
    Run the fastest checks first before doing function calls
    
    Change-Id: I6a012ad2d8d35bbfa6593454c3251c98e12f6f52
    Reviewed-on: http://review.couchbase.org/34892
    Tested-by: buildbot <build@couchbase.com>
    Reviewed-by: Trond Norbye <trond.norbye@gmail.com>

--- src/cJSON.c
+++ src/cJSON.c
@@ -248,13 +248,13 @@ void cJSON_Free(char *ptr) { free(ptr); }
 static const char *parse_value(cJSON *item,const char *value)
 {
     if (!value)                     return 0;   /* Fail on null. */
-    if (!strncmp(value,"null",4))   { item->type=cJSON_NULL;  return value+4; }
-    if (!strncmp(value,"false",5))  { item->type=cJSON_False; return value+5; }
-    if (!strncmp(value,"true",4))   { item->type=cJSON_True; item->valueint=1;  return value+4; }
     if (*value=='\"')               { return parse_string(item,value); }
     if (*value=='-' || (*value>='0' && *value<='9'))    { return parse_number(item,value); }
     if (*value=='[')                { return parse_array(item,value); }
     if (*value=='{')                { return parse_object(item,value); }
+    if (!strncmp(value,"null",4))   { item->type=cJSON_NULL;  return value+4; }
+    if (!strncmp(value,"false",5))  { item->type=cJSON_False; return value+5; }
+    if (!strncmp(value,"true",4))   { item->type=cJSON_True; item->valueint=1;  return value+4; }
 
     return 0;   /* failure. */
 }
