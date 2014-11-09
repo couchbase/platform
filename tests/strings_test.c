98aca6173594007488da1b7f7f462992242f3e4e
Trond Norbye <trond.norbye@gmail.com>
2014-11-09 13:39:43 +0100

    Mute compile warning from gcc
    Change-Id: If9babaf57436ce0b107d01dd1017b4a4b5ccd13d
    Reviewed-on: http://review.couchbase.org/43022
    Reviewed-by: Trond Norbye <trond.norbye@gmail.com>
    Tested-by: Trond Norbye <trond.norbye@gmail.com>

--- tests/strings_test.c
+++ tests/strings_test.c
@@ -7,15 +7,15 @@
 
 static void test_asprintf(void) {
     char *result = 0;
-    asprintf(&result, "test 1");
+    (void)asprintf(&result, "test 1");
     cb_assert(strcmp(result, "test 1") == 0);
     free(result);
 
-    asprintf(&result, "test %d", 2);
+    (void)asprintf(&result, "test %d", 2);
     cb_assert(strcmp(result, "test 2") == 0);
     free(result);
 
-    asprintf(&result, "%c%c%c%c %d", 't', 'e', 's', 't', 3);
+    (void)asprintf(&result, "%c%c%c%c %d", 't', 'e', 's', 't', 3);
     cb_assert(strcmp(result, "test 3") == 0);
     free(result);
 }
