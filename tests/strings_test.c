97c8ae2e5624cdd8576a9acc9b70a9cb7cb47927
Trond Norbye <trond.norbye@gmail.com>
2014-11-09 17:09:39 +0100

    Check for return value for asprintf
    Change-Id: Ib163468aac1a5e52cef28c59b55be2287dc4ba43
    Reviewed-on: http://review.couchbase.org/43023
    Reviewed-by: Trond Norbye <trond.norbye@gmail.com>
    Tested-by: Trond Norbye <trond.norbye@gmail.com>

--- tests/strings_test.c
+++ tests/strings_test.c
@@ -7,15 +7,15 @@
 
 static void test_asprintf(void) {
     char *result = 0;
-    (void)asprintf(&result, "test 1");
+    cb_assert(asprintf(&result, "test 1") > 0);
     cb_assert(strcmp(result, "test 1") == 0);
     free(result);
 
-    (void)asprintf(&result, "test %d", 2);
+    cb_assert(asprintf(&result, "test %d", 2) > 0);
     cb_assert(strcmp(result, "test 2") == 0);
     free(result);
 
-    (void)asprintf(&result, "%c%c%c%c %d", 't', 'e', 's', 't', 3);
+    cb_assert(asprintf(&result, "%c%c%c%c %d", 't', 'e', 's', 't', 3) > 0);
     cb_assert(strcmp(result, "test 3") == 0);
     free(result);
 }
