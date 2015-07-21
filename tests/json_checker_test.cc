d3855e552a21be8bb5503103691aa65ec8fe53a0
Dave Rigby <daver@couchbase.com>
2015-07-21 14:44:46 +0100

    MB-16604 [BP 3.x]: Fix JSON_checker leak upon invalid UTF-8
    A memory leak has been identified in the checkUTF8JSON function which
    is used to determine the datatype field for document
    mutations. Specifically, the leak occurs when validating UTF8 contents
    of a JSON document. If an invalid UTF8 escape sequence is encountered
    then we correctly mark the document as "binary" (non-JSON), however we
    leak 52 bytes.
    
    This is due to an error in the JSON_checker cleanup code - it will
    free that memory if the actual JSON structure was invalid, but not if
    it only encountered a bad UTF-8 value.
    
    In general any document which:
    1. Contains a byte of '"' (i.e. ASCII 0x22)
    2. Followed (not necessary immediately) by 0xf8...0xff
    3. And hasn't already failed JSON validation
    
    could trigger this issue.
    
    Change-Id: I6ff54242a7427f520891446cb597a9e3dd5188e1
    Reviewed-on: http://review.couchbase.org/56385
    Tested-by: buildbot <build@couchbase.com>
    Reviewed-by: Jim Walker <jim@couchbase.com>
    Reviewed-by: Trond Norbye <trond.norbye@gmail.com>

--- tests/json_checker_test.cc
+++ tests/json_checker_test.cc
@@ -33,4 +33,16 @@ int main(void) {
     check(CHECK_JSON("null"), "bare values are OK");
     check(CHECK_JSON("99"), "bare numbers are OK");
     check(!CHECK_JSON("{\"test\xFF\": 12}"), "bad UTF-8 is not OK");
+
+    // MB-15778: Regression test for memory leaks.
+    unsigned char mb15778_1[] = {'"', 0xff, 0};
+    check(!CHECK_JSON(mb15778_1), "bad UTF-8 is not OK");
+    unsigned char mb15778_2[] = {'"', 'a', 0xff, 0};
+    check(!CHECK_JSON(mb15778_2), "bad UTF-8 is not OK");
+    unsigned char mb15778_3[] = {'"', '1', '2', 0xfe, 0};
+    check(!CHECK_JSON(mb15778_3), "bad UTF-8 is not OK");
+    unsigned char mb15778_4[] = {'"', '1', '2', 0xfd, 0};
+    check(!CHECK_JSON(mb15778_4), "bad UTF-8 is not OK");
+    unsigned char mb15778_5[] = {'{', '"', 'k', '"', ':', '"', 0xfc, 0};
+    check(!CHECK_JSON(mb15778_5), "bad UTF-8 is not OK");
 }
