b7be8424539d93379b87ee007b76687f76500640
Dave Rigby <daver@couchbase.com>
2015-07-29 16:32:33 +0100

    Add CB_PRINTF_FORMAT function decoractor
    Add a new macro CB_PRINTF_FORMAT to be used to decorate functions
    whose arguments are similar to printf. When support in the compiler is
    present this allows compile-time checking that the arguments to such
    functions match the format specifier.
    
    Change-Id: I9fa51e97b508c0a97a4e3028097a8e3be403854a
    Reviewed-on: http://review.couchbase.org/53876
    Tested-by: buildbot <build@couchbase.com>
    Reviewed-by: Jim Walker <jim@couchbase.com>

--- include/platform/dynamic.in.h
+++ include/platform/dynamic.in.h
@@ -14,3 +14,12 @@
 #else
 #define CB_MUST_USE_RESULT
 #endif
+
+/* Function attribute to be used for printf-style format checking
+ */
+#cmakedefine HAVE_ATTR_FORMAT
+#if defined(HAVE_ATTR_FORMAT)
+#define CB_FORMAT_PRINTF(FMT_IDX, STR_IDX) __attribute__((format(printf, FMT_IDX, STR_IDX)))
+#else
+#define CB_FORMAT_PRINTF(FMT_IDX, STR_IDX)
+#endif
