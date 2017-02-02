1f2f60b6139089618543a49044b0d3c1744f5a2d
James Harrison <00jamesh@gmail.com>
2017-02-02 13:04:30 +0000

    MB-22230 add NonNegativeCounter<>
    Creating NonNegativeCounter<> which cannot be reduced below 0, and will
    instead be clamped at 0.
    
    This simplifies some code e.g., we currently use
    
     stats.diskQueueSize.fetch_add
     stats.decrDiskQueueSize
    
    because using
    
     stats.diskQueueSize.fetch_sub
    
    directly could underflow a std::atomic.
    
    Change-Id: I19e8645e5c1d4c608976750b323c825a1ec1b98b
    Reviewed-on: http://review.couchbase.org/72409
    Reviewed-by: Dave Rigby <daver@couchbase.com>
    Reviewed-by: Trond Norbye <trond.norbye@gmail.com>
    Tested-by: Build Bot <build@couchbase.com>

---
+++ include/platform/non_negative_counter.h
@@ -0,0 +1,135 @@
+/* -*- Mode: C++; tab-width: 4; c-basic-offset: 4; indent-tabs-mode: nil -*- */
+/*
+ *     Copyright 2017 Couchbase, Inc.
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+#pragma once
+
+#include <atomic>
+#include <type_traits>
+
+namespace cb {
+
+/**
+ * The NonNegativeCounter class wraps std::atomic<> and prevents it
+ * underflowing, instead clamping the value at 0
+ */
+template <typename T>
+class NonNegativeCounter {
+    static_assert(
+            std::is_unsigned<T>::value,
+            "NonNegativeCounter should only be templated over unsigned types");
+
+public:
+    NonNegativeCounter() {
+        store(0);
+    }
+
+    NonNegativeCounter(const T& initial) {
+        store(initial);
+    }
+
+    explicit NonNegativeCounter(const NonNegativeCounter& other) {
+        store(other.load());
+    }
+
+    operator T() const {
+        return load();
+    }
+
+    T load() const {
+        return value.load(std::memory_order_relaxed);
+    }
+
+    void store(T desired) {
+        value.store(desired, std::memory_order_relaxed);
+    }
+
+    T fetch_add(T arg) {
+        return value.fetch_add(arg, std::memory_order_relaxed);
+    }
+
+    T fetch_sub(T arg) {
+        T expected = load();
+        T desired;
+        do {
+            if (expected < arg) {
+                desired = 0;
+            } else {
+                desired = expected - arg;
+            }
+        } while (!value.compare_exchange_weak(
+                expected, desired, std::memory_order_relaxed));
+
+        return expected;
+    }
+
+    T exchange(T arg) {
+        return value.exchange(arg, std::memory_order_relaxed);
+    }
+
+    NonNegativeCounter& operator=(const NonNegativeCounter& rhs) {
+        store(rhs.load());
+        return *this;
+    }
+
+    NonNegativeCounter& operator+=(const T rhs) {
+        fetch_add(rhs);
+        return *this;
+    }
+
+    NonNegativeCounter& operator+=(const NonNegativeCounter& rhs) {
+        fetch_add(rhs.load());
+        return *this;
+    }
+
+    NonNegativeCounter& operator-=(const T rhs) {
+        fetch_sub(rhs);
+        return *this;
+    }
+
+    NonNegativeCounter& operator-=(const NonNegativeCounter& rhs) {
+        fetch_sub(rhs.load());
+        return *this;
+    }
+
+    T operator++() {
+        return fetch_add(1) + 1;
+    }
+
+    T operator++(int) {
+        return fetch_add(1);
+    }
+
+    T operator--() {
+        T previous = fetch_sub(1);
+        if (previous == 0) {
+            return 0;
+        }
+        return previous - 1;
+    }
+
+    T operator--(int) {
+        return fetch_sub(1);
+    }
+
+    NonNegativeCounter& operator=(T val) {
+        store(val);
+        return *this;
+    }
+
+private:
+    std::atomic<T> value;
+};
+}
