53e6ec72fc692f805eb5f307fafe8aaaa4c25830
Dave Rigby <daver@couchbase.com>
2020-06-22 13:57:50 +0100

    Allow AtomicDuration to be parameterised on duration type
    Change AtomicDuration<> template to allow the duration in use to be
    templated, instead of being hard-coded to
    std::chrono::steady_clock::duration.
    
    Change-Id: I4c2bc48b43a1468160fa06b60daf59dc37d7c9c9
    Reviewed-on: http://review.couchbase.org/c/platform/+/131100
    Reviewed-by: James Harrison <james.harrison@couchbase.com>
    Reviewed-by: Trond Norbye <trond.norbye@couchbase.com>
    Tested-by: Build Bot <build@couchbase.com>

--- include/platform/atomic_duration.h
+++ include/platform/atomic_duration.h
@@ -24,17 +24,21 @@ namespace cb {
 /* Wrapper class around std::atomic. It provides atomic operations
  * for std::chrono durations by operating on the underlying value
  * obtained from the duration's count().
- * Defaults to relaxed memory ordering, suitable for statistics.
+ * @tparam Duration The duration type to use, defaults to steady_clock
+ *         duration.
+ * @tparam MemoryOrder The memory ordering to use for atomic operations,
+ *         defaults to relaxed memory ordering, suitable for statistics.
  */
-template <std::memory_order MemoryOrder =
+template <typename Duration = std::chrono::steady_clock::duration,
+          std::memory_order MemoryOrder =
                   std::memory_order::memory_order_relaxed>
 class AtomicDuration {
 public:
     AtomicDuration() {
-        store(std::chrono::steady_clock::duration::zero());
+        store(Duration::zero());
     }
 
-    AtomicDuration(std::chrono::steady_clock::duration initial) {
+    AtomicDuration(Duration initial) {
         store(initial);
     }
 
@@ -42,64 +46,58 @@ public:
         store(other.load());
     }
 
-    operator std::chrono::steady_clock::duration() const {
+    operator Duration() const {
         return load();
     }
 
-    [[nodiscard]] std::chrono::steady_clock::duration load() const {
-        return std::chrono::steady_clock::duration(value.load(MemoryOrder));
+    [[nodiscard]] Duration load() const {
+        return Duration(value.load(MemoryOrder));
     }
 
-    void store(std::chrono::steady_clock::duration desired) {
+    void store(Duration desired) {
         value.store(desired.count(), MemoryOrder);
     }
 
-    std::chrono::steady_clock::duration fetch_add(
-            std::chrono::steady_clock::duration arg) {
-        return std::chrono::steady_clock::duration(
-                value.fetch_add(arg.count(), MemoryOrder));
+    Duration fetch_add(Duration arg) {
+        return Duration(value.fetch_add(arg.count(), MemoryOrder));
     }
 
-    std::chrono::steady_clock::duration fetch_sub(
-            std::chrono::steady_clock::duration arg) {
-        return std::chrono::steady_clock::duration(
-                value.fetch_sub(arg.count(), MemoryOrder));
+    Duration fetch_sub(Duration arg) {
+        return Duration(value.fetch_sub(arg.count(), MemoryOrder));
     }
 
-    AtomicDuration& operator=(std::chrono::steady_clock::duration val) {
+    AtomicDuration& operator=(Duration val) {
         store(val);
         return *this;
     }
 
-    AtomicDuration& operator+=(std::chrono::steady_clock::duration rhs) {
+    AtomicDuration& operator+=(Duration rhs) {
         fetch_add(rhs);
         return *this;
     }
 
-    AtomicDuration& operator-=(std::chrono::steady_clock::duration rhs) {
+    AtomicDuration& operator-=(Duration rhs) {
         fetch_sub(rhs);
         return *this;
     }
 
-    std::chrono::steady_clock::duration operator++() {
-        return fetch_add(std::chrono::steady_clock::duration(1)) +
-               std::chrono::steady_clock::duration(1);
+    Duration operator++() {
+        return fetch_add(Duration(1)) + Duration(1);
     }
 
-    std::chrono::steady_clock::duration operator++(int) {
-        return fetch_add(std::chrono::steady_clock::duration(1));
+    Duration operator++(int) {
+        return fetch_add(Duration(1));
     }
 
-    std::chrono::steady_clock::duration operator--() {
-        return fetch_sub(std::chrono::steady_clock::duration(1)) -
-               std::chrono::steady_clock::duration(1);
+    Duration operator--() {
+        return fetch_sub(Duration(1)) - Duration(1);
     }
 
-    std::chrono::steady_clock::duration operator--(int) {
-        return fetch_sub(std::chrono::steady_clock::duration(1));
+    Duration operator--(int) {
+        return fetch_sub(Duration(1));
     }
 
 private:
-    std::atomic<std::chrono::steady_clock::duration::rep> value;
+    std::atomic<typename Duration::rep> value;
 };
 } // namespace cb
