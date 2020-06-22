a34c839c992c720a0bc79f60a4e75bd686f6b683
Dave Rigby <daver@couchbase.com>
2020-06-22 15:15:38 +0100

    Introduce AtomicTimePoint template class
    Add a new template class - AtomicTimePoint<>. This is similar to
    AtomicDuration in that it provides an atomic, lock-tree class
    representing a time_point from a particular clock.
    
    Change-Id: I89a11b0ad651f7f235a296c027d7ab1307235161
    Reviewed-on: http://review.couchbase.org/c/platform/+/131159
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Jim Walker <jim@couchbase.com>

--- include/platform/atomic_duration.h
+++ include/platform/atomic_duration.h
@@ -106,4 +106,52 @@ private:
             "don't need a lock-free "
             "atomic duration, std::atomic<Duration> might as well be used.");
 };
+
+/* Wrapper class around std::atomic. It provides atomic operations
+ * for std::chrono time_points by operating on the underlying value
+ * obtained from the time_point's representation..
+ * @tparam Clock The time_point type to use, defaults to steady_clock time_point.
+ * @tparam MemoryOrder The memory ordering to use for atomic operations,
+ *         defaults to relaxed memory ordering, suitable for statistics.
+ */
+template <typename TimePoint = std::chrono::steady_clock::time_point,
+        std::memory_order MemoryOrder =
+        std::memory_order::memory_order_relaxed>
+class AtomicTimePoint {
+public:
+    AtomicTimePoint() {
+        store(TimePoint{});
+    }
+
+    AtomicTimePoint(TimePoint initial) {
+        store(initial);
+    }
+
+    AtomicTimePoint& operator=(TimePoint val) {
+        store(val);
+        return *this;
+    }
+
+    operator TimePoint() const {
+        return load();
+    }
+
+    [[nodiscard]] TimePoint load() const {
+        return TimePoint{typename TimePoint::duration{value.load(MemoryOrder)}};
+    }
+
+    void store(TimePoint desired) {
+        value.store(desired.time_since_epoch().count(), MemoryOrder);
+    }
+
+private:
+    std::atomic<typename TimePoint::rep> value;
+
+    static_assert(
+            decltype(value)::is_always_lock_free,
+            "This instantiation of AtomicTimePoint<> is not lock-free. If you "
+            "don't need a lock-free atomic duration, std::atomic<TimePoint> "
+            "might as well be used.");
+};
+
 } // namespace cb
