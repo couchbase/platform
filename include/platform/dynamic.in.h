d1b8831c553054dd7caeac44db2e55b92617a63b
Trond Norbye <trond.norbye@gmail.com>
2021-05-07 15:37:50 +0200

    Remove unused macro CB_MUST_USE_RESULT
    Use [[nodiscard]] instead if you need it
    
    Change-Id: I80118dd1fce708bd8831dda92abebd32d393910f
    Reviewed-on: http://review.couchbase.org/c/platform/+/153139
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Richard de Mellow <richard.demellow@couchbase.com>

--- include/platform/dynamic.in.h
+++ include/platform/dynamic.in.h
@@ -2,17 +2,6 @@
 
 #cmakedefine CB_DONT_NEED_BYTEORDER 1
 
-/* Function attribute to be used where the return value should not be
- * ignored by the caller. For example: allocation functions where the
- * caller should free the result after use.
- */
-#cmakedefine HAVE_ATTR_WARN_UNUSED_RESULT
-#if defined(HAVE_ATTR_WARN_UNUSED_RESULT)
-#define CB_MUST_USE_RESULT __attribute__((warn_unused_result))
-#else
-#define CB_MUST_USE_RESULT
-#endif
-
 /* Function attribute to be used for printf-style format checking
  */
 #cmakedefine HAVE_ATTR_FORMAT
