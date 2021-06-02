ee41e81d84a6fdab57bdecfb7c9c8e61f7490b1a
Trond Norbye <trond.norbye@gmail.com>
2021-06-02 07:48:17 +0200

    Remove double file extension for sysinfo_test.cc
    Change-Id: I3cf70c7ab2edb8eba491a70615b4084f662f2471
    Reviewed-on: http://review.couchbase.org/c/platform/+/154896
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Dave Rigby <daver@couchbase.com>

--- tests/sysinfo/sysinfo_test.cc
+++ tests/sysinfo/sysinfo_test.cc
@@ -9,8 +9,8 @@
  */
 #include <folly/portability/GTest.h>
 
-#include <platform/sysinfo.h>
 #include <platform/checked_snprintf.h>
+#include <platform/sysinfo.h>
 
 char cpu_count_env_var[256];
 
@@ -19,57 +19,66 @@ TEST(GetAvailableCpu, NoVariable) {
 }
 
 TEST(GetAvailableCpu, CorrectVariable_ExactNumber) {
-    checked_snprintf(cpu_count_env_var, sizeof(cpu_count_env_var),
+    checked_snprintf(cpu_count_env_var,
+                     sizeof(cpu_count_env_var),
                      "COUCHBASE_CPU_COUNT=10000");
     putenv(cpu_count_env_var);
     EXPECT_EQ(10000u, cb::get_available_cpu_count());
 }
 
 TEST(GetAvailableCpu, CorrectVariable_LeadingSpace) {
-    checked_snprintf(cpu_count_env_var, sizeof(cpu_count_env_var),
+    checked_snprintf(cpu_count_env_var,
+                     sizeof(cpu_count_env_var),
                      "COUCHBASE_CPU_COUNT= 9999");
     putenv(cpu_count_env_var);
     EXPECT_EQ(9999u, cb::get_available_cpu_count());
 }
 
 TEST(GetAvailableCpu, CorrectVariable_TrailingSpace) {
-    checked_snprintf(cpu_count_env_var, sizeof(cpu_count_env_var),
+    checked_snprintf(cpu_count_env_var,
+                     sizeof(cpu_count_env_var),
                      "COUCHBASE_CPU_COUNT=9998 ");
     putenv(cpu_count_env_var);
     EXPECT_EQ(9998u, cb::get_available_cpu_count());
 }
 
 TEST(GetAvailableCpu, CorrectVariable_LeadingTab) {
-    checked_snprintf(cpu_count_env_var, sizeof(cpu_count_env_var),
+    checked_snprintf(cpu_count_env_var,
+                     sizeof(cpu_count_env_var),
                      "COUCHBASE_CPU_COUNT=\t9997");
     putenv(cpu_count_env_var);
     EXPECT_EQ(9997u, cb::get_available_cpu_count());
 }
 
 TEST(GetAvailableCpu, CorrectVariable_TrailigTab) {
-    checked_snprintf(cpu_count_env_var, sizeof(cpu_count_env_var),
+    checked_snprintf(cpu_count_env_var,
+                     sizeof(cpu_count_env_var),
                      "COUCHBASE_CPU_COUNT=9996\t");
     putenv(cpu_count_env_var);
     EXPECT_EQ(9996u, cb::get_available_cpu_count());
 }
 
 TEST(GetAvailableCpu, InvalidValue) {
-    checked_snprintf(cpu_count_env_var, sizeof(cpu_count_env_var),
+    checked_snprintf(cpu_count_env_var,
+                     sizeof(cpu_count_env_var),
                      "COUCHBASE_CPU_COUNT=1a");
     putenv(cpu_count_env_var);
     EXPECT_THROW(cb::get_available_cpu_count(), std::logic_error);
 
-    checked_snprintf(cpu_count_env_var, sizeof(cpu_count_env_var),
+    checked_snprintf(cpu_count_env_var,
+                     sizeof(cpu_count_env_var),
                      "COUCHBASE_CPU_COUNT=1 a");
     putenv(cpu_count_env_var);
     EXPECT_THROW(cb::get_available_cpu_count(), std::logic_error);
 
-    checked_snprintf(cpu_count_env_var, sizeof(cpu_count_env_var),
+    checked_snprintf(cpu_count_env_var,
+                     sizeof(cpu_count_env_var),
                      "COUCHBASE_CPU_COUNT=a1");
     putenv(cpu_count_env_var);
     EXPECT_THROW(cb::get_available_cpu_count(), std::logic_error);
 
-    checked_snprintf(cpu_count_env_var, sizeof(cpu_count_env_var),
+    checked_snprintf(cpu_count_env_var,
+                     sizeof(cpu_count_env_var),
                      "COUCHBASE_CPU_COUNT=a 1");
     putenv(cpu_count_env_var);
     EXPECT_THROW(cb::get_available_cpu_count(), std::logic_error);
