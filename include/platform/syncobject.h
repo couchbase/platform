e6e5e1f041b6cb46b4d5158f1a345a8feda4e8d6
Trond Norbye <trond.norbye@gmail.com>
2021-06-04 09:43:29 +0200

    MB-46729: Add syncobject.h (moved from ep-engine source)
    As part of promoting the executor from ep-engine to the core
    we need syncobject.h in a more general location. It doesn't contain
    any "ep-engine" specific parts so it can just live in platform.
    
    Change-Id: I547a67ebe1341be3e4ec6a6feec2eb56e76a0350
    Reviewed-on: http://review.couchbase.org/c/platform/+/155080
    Reviewed-by: Dave Rigby <daver@couchbase.com>
    Tested-by: Build Bot <build@couchbase.com>

---
+++ include/platform/syncobject.h
@@ -0,0 +1,62 @@
+/*
+ *     Copyright 2015-Present Couchbase, Inc.
+ *
+ *   Use of this software is governed by the Business Source License included
+ *   in the file licenses/BSL-Couchbase.txt.  As of the Change Date specified
+ *   in that file, in accordance with the Business Source License, use of this
+ *   software will be governed by the Apache License, Version 2.0, included in
+ *   the file licenses/APL2.txt.
+ */
+#pragma once
+
+#include <chrono>
+#include <condition_variable>
+
+/**
+ * Abstraction built on top of std::condition_variable & std::mutex
+ */
+class SyncObject : public std::mutex {
+public:
+    SyncObject() = default;
+    SyncObject(const SyncObject&) = delete;
+    void operator=(const SyncObject&) = delete;
+    ~SyncObject() = default;
+
+    // Note: there is no `wait(lock)` override (without Predicate) provided.
+    // This is because the underlying std::condition_variable::wait(lock)
+    // method can get spurious wakeups, and hence can miss notifications if
+    // using a non-predicated wait - see
+    // http://isocpp.github.io/CppCoreGuidelines/CppCoreGuidelines#Rconc-wait
+
+    template <class Predicate>
+    void wait(std::unique_lock<std::mutex>& lock, Predicate pred) {
+        cond.wait(lock, pred);
+    }
+
+    void wait_for(std::unique_lock<std::mutex>& lock, const double secs) {
+        cond.wait_for(lock, std::chrono::milliseconds(int64_t(secs * 1000.0)));
+    }
+
+    void wait_for(std::unique_lock<std::mutex>& lock,
+                  const std::chrono::nanoseconds nanoSecs) {
+        cond.wait_for(lock, nanoSecs);
+    }
+
+    template <class Predicate>
+    void wait_for(std::unique_lock<std::mutex>& lock,
+                  const std::chrono::nanoseconds nanoSecs,
+                  Predicate pred) {
+        cond.wait_for(lock, nanoSecs, pred);
+    }
+
+    void notify_all() {
+        cond.notify_all();
+    }
+
+    void notify_one() {
+        cond.notify_one();
+    }
+
+protected:
+    std::condition_variable cond;
+};
