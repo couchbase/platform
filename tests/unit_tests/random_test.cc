3638d02084fa1e1c82ef398616dbdd593682fe77
Trond Norbye <trond.norbye@gmail.com>
2021-06-15 18:06:33 +0200

    Move random_test.cc to the unit test batch
    Change-Id: Ibaa680ff6eefc3b4c1af8e8e612b41e8d47677fe
    Reviewed-on: http://review.couchbase.org/c/platform/+/155843
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Jim Walker <jim@couchbase.com>

---
+++ tests/unit_tests/random_test.cc
@@ -0,0 +1,35 @@
+/*
+ *     Copyright 2014-Present Couchbase, Inc.
+ *
+ *   Use of this software is governed by the Business Source License included
+ *   in the file licenses/BSL-Couchbase.txt.  As of the Change Date specified
+ *   in that file, in accordance with the Business Source License, use of this
+ *   software will be governed by the Apache License, Version 2.0, included in
+ *   the file licenses/APL2.txt.
+ */
+#include <folly/portability/GTest.h>
+#include <platform/random.h>
+
+TEST(RandomGenerator, RandomTest) {
+    cb::RandomGenerator r1;
+    cb::RandomGenerator r2;
+
+    uint64_t v1 = r1.next();
+    uint64_t v2 = r2.next();
+    EXPECT_NE(v1, v2) << "I did not expect the random generators to return the"
+                      << " same value";
+
+    std::array<char, 1024> buffer;
+    std::fill(buffer.begin(), buffer.end(), 0);
+    if (r1.getBytes(buffer.data(), buffer.size())) {
+        size_t ii;
+
+        for (ii = 0; ii < buffer.size(); ++ii) {
+            if (buffer[ii] != 0) {
+                break;
+            }
+        }
+
+        EXPECT_NE(buffer.size(), ii) << "I got 1k of 0 (or it isn't working)";
+    }
+}
