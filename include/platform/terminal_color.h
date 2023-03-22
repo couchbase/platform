7ff6c97aeb38d88cb8758eaf7ba37d7727ccdd08
Trond Norbye <trond.norbye@gmail.com>
2023-03-22 12:45:19 +0100

    Refactor: Move terminal size/color from kv_engine
    Move the functionality to get terminal size and colors
    from kv_engine into platform and put them in cb::terminal
    namespace
    
    Change-Id: Ibed8445f1751e65c45fb80e079b765ebb8d34093
    Reviewed-on: https://review.couchbase.org/c/platform/+/188592
    Tested-by: Trond Norbye <trond.norbye@couchbase.com>
    Reviewed-by: Paolo Cocchi <paolo.cocchi@couchbase.com>

---
+++ include/platform/terminal_color.h
@@ -0,0 +1,38 @@
+/*
+ *    Copyright 2021-Present Couchbase, Inc.
+ *
+ *   Use of this software is governed by the Business Source License included
+ *   in the file licenses/BSL-Couchbase.txt.  As of the Change Date specified
+ *   in that file, in accordance with the Business Source License, use of this
+ *   software will be governed by the Apache License, Version 2.0, included in
+ *   the file licenses/APL2.txt.
+ */
+
+#pragma once
+
+#include <ostream>
+
+namespace cb::terminal {
+enum class TerminalColor {
+    Black,
+    Red,
+    Green,
+    Yellow,
+    Blue,
+    Magenta,
+    Cyan,
+    White,
+    Reset
+};
+
+/// Enable/disable support for using colors. When enabled the << operator
+/// will print out the escape sequence to set the font color. When disabled
+/// the operator don't insert anything to the stream.
+void setTerminalColorSupport(bool enable);
+
+/// Is terminal color currently enabled
+bool isTerminalColorEnabled();
+
+/// Insert the formatting code the requested color to the stream
+std::ostream& operator<<(std::ostream& os, const TerminalColor& color);
+} // namespace cb::terminal
