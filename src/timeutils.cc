28c244602433c5799a48d8caf62f7c7d74b0b7f9
Trond Norbye <trond.norbye@gmail.com>
2024-12-19 09:47:20 +0100

    Use std::abs instead of abs as std::abs contains overloads for long
    Change-Id: I88ba935508c6142b52d74c56f7b339b6ee924a8b
    Reviewed-on: https://review.couchbase.org/c/platform/+/221201
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Mohammad Zaeem <mohammad.zaeem@couchbase.com>

--- src/timeutils.cc
+++ src/timeutils.cc
@@ -67,7 +67,7 @@ std::string timestamp(std::chrono::nanoseconds time_since_epoc) {
 std::string cb::time2text(std::chrono::nanoseconds time2convert) {
     const char* const extensions[] = {" ns", " us", " ms", " s", nullptr};
     int id = 0;
-    double time = abs(time2convert.count());
+    double time = std::abs(time2convert.count());
 
     while (time > 9999) {
         ++id;
