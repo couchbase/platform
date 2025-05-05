03722e9b2af6f9d465e7b545679abaf6e8f847f3
Trond Norbye <trond.norbye@gmail.com>
2025-05-05 12:37:59 +0200

    Revert "MB-60861: Remove ThreadLocalMonotonicResource"
    This reverts commit 5995f93f0ded429570e801b33543c9119cec6672.
    
    The patch cause kv_engine CV til fail
    
    Change-Id: I88cc74a0fff160c45d41cebda129a7793dfed6ad
    Reviewed-on: https://review.couchbase.org/c/platform/+/227286
    Reviewed-by: Trond Norbye <trond.norbye@couchbase.com>
    Tested-by: Trond Norbye <trond.norbye@couchbase.com>

--- tests/benchmarks/json_log_bench.cc
+++ tests/benchmarks/json_log_bench.cc
@@ -15,7 +15,7 @@
 
 static void BM_Logger_LogJson(benchmark::State& state) {
     while (state.KeepRunning()) {
-        cb::logger::Json x{{"pi", 3.14},
+        cb::logger::Json x{{"pi", 3.141},
                            {"happy", true},
                            {"name", "Niels"},
                            {"nothing", nullptr},
@@ -30,7 +30,7 @@ BENCHMARK(BM_Logger_LogJson);
 static void BM_Logger_NlohmannOrdered(benchmark::State& state) {
     while (state.KeepRunning()) {
         nlohmann::ordered_json x{
-                {"pi", 3.14},
+                {"pi", 3.141},
                 {"happy", true},
                 {"name", "Niels"},
                 {"nothing", nullptr},
@@ -44,7 +44,7 @@ BENCHMARK(BM_Logger_NlohmannOrdered);
 
 static void BM_Logger_NlohmannJson(benchmark::State& state) {
     while (state.KeepRunning()) {
-        nlohmann::json x{{"pi", 3.14},
+        nlohmann::json x{{"pi", 3.141},
                          {"happy", true},
                          {"name", "Niels"},
                          {"nothing", nullptr},
@@ -55,50 +55,3 @@ static void BM_Logger_NlohmannJson(benchmark::State& state) {
     }
 }
 BENCHMARK(BM_Logger_NlohmannJson);
-
-// Similar to what we do in KV to merge the prefix with the context.
-template <typename JsonType>
-void mergeContext(JsonType& dest, JsonType src) {
-    auto& destObj = dest.template get_ref<typename JsonType::object_t&>();
-    auto& srcObj = src.template get_ref<typename JsonType::object_t&>();
-    std::move(srcObj.begin(), srcObj.end(), std::back_inserter(destObj));
-}
-
-template <typename JsonType>
-void dumpWithPrefix(const nlohmann::ordered_json& prefix, JsonType ctx) {
-    auto finalCtx = JsonType(prefix);
-    mergeContext(finalCtx, std::move(ctx));
-    (void)finalCtx.dump();
-}
-
-// Test merging with our own JsonType.
-static void BM_Logger_Merge_LogJson(benchmark::State& state) {
-    const nlohmann::ordered_json prefix{
-            {"pi", 3.14}, {"happy", true}, {"name", "Niels"}};
-    while (state.KeepRunning()) {
-        dumpWithPrefix(
-                prefix,
-                cb::logger::BasicJsonType{
-                        {"nothing", nullptr},
-                        {"answer", {{"everything", 42}}},
-                        {"list", {1, 0, 2}},
-                        {"object", {{"currency", "USD"}, {"value", 42.99}}}});
-    }
-}
-BENCHMARK(BM_Logger_Merge_LogJson);
-
-// Test merging with nlohmann::ordered_json.
-static void BM_Logger_Merge_NlohmannOrdered(benchmark::State& state) {
-    const nlohmann::ordered_json prefix{
-            {"pi", 3.14}, {"happy", true}, {"name", "Niels"}};
-    while (state.KeepRunning()) {
-        dumpWithPrefix(
-                prefix,
-                nlohmann::ordered_json{
-                        {"nothing", nullptr},
-                        {"answer", {{"everything", 42}}},
-                        {"list", {1, 0, 2}},
-                        {"object", {{"currency", "USD"}, {"value", 42.99}}}});
-    }
-}
-BENCHMARK(BM_Logger_Merge_NlohmannOrdered);
---
+++ tests/unit_tests/thread_local_monotonic_resource_test.cc
@@ -0,0 +1,136 @@
+/*
+ *     Copyright 2024-Present Couchbase, Inc.
+ *
+ *   Use of this software is governed by the Business Source License included
+ *   in the file licenses/BSL-Couchbase.txt.  As of the Change Date specified
+ *   in that file, in accordance with the Business Source License, use of this
+ *   software will be governed by the Apache License, Version 2.0, included in
+ *   the file licenses/APL2.txt.
+ */
+#include <folly/portability/GTest.h>
+
+#include <gtest/gtest.h>
+#include <platform/cb_arena_malloc.h>
+#include <platform/cb_malloc.h>
+#include <platform/thread_local_monotonic_resource.h>
+#include <new>
+#include <thread>
+
+/// Check that we can allocate successfully and the counters get updated.
+TEST(ThreadLocalMonotonicResource, Basic) {
+    struct Tag {};
+    using Resource = cb::ThreadLocalMonotonicResource<Tag,
+                                                      10 * sizeof(int),
+                                                      10 * sizeof(int)>;
+    using Allocator = Resource::Allocator<int>;
+
+    Allocator a;
+    auto* ptr = a.allocate(5);
+
+    EXPECT_EQ(5 * sizeof(int), a.getUnderlyingBuffer().getAllocatedBytes());
+    EXPECT_EQ(5 * sizeof(int), a.getUnderlyingBuffer().getMaxAllocatedBytes());
+    EXPECT_EQ(1, a.getUnderlyingBuffer().getAllocationCount());
+    EXPECT_EQ(1, a.getUnderlyingBuffer().getMaxAllocationCount());
+    a.deallocate(ptr, 5);
+    EXPECT_EQ(0, a.getUnderlyingBuffer().getAllocatedBytes());
+    EXPECT_EQ(5 * sizeof(int), a.getUnderlyingBuffer().getMaxAllocatedBytes());
+    EXPECT_EQ(0, a.getUnderlyingBuffer().getAllocationCount());
+    EXPECT_EQ(1, a.getUnderlyingBuffer().getMaxAllocationCount());
+}
+
+/// Check that we can make multiple allocations
+TEST(ThreadLocalMonotonicResource, MultipleAllocs) {
+    struct Tag {};
+    using Resource = cb::ThreadLocalMonotonicResource<Tag,
+                                                      10 * sizeof(int),
+                                                      20 * sizeof(int)>;
+    using Allocator = Resource::Allocator<int>;
+
+    Allocator a;
+    auto* ptr = a.allocate(15);
+    auto* ptr2 = a.allocate(5);
+
+    EXPECT_EQ(20 * sizeof(int), a.getUnderlyingBuffer().getAllocatedBytes());
+    EXPECT_EQ(20 * sizeof(int), a.getUnderlyingBuffer().getMaxAllocatedBytes());
+    EXPECT_EQ(2, a.getUnderlyingBuffer().getAllocationCount());
+    EXPECT_EQ(2, a.getUnderlyingBuffer().getMaxAllocationCount());
+
+    a.deallocate(ptr, 15);
+    EXPECT_EQ(5 * sizeof(int), a.getUnderlyingBuffer().getAllocatedBytes());
+    EXPECT_EQ(20 * sizeof(int), a.getUnderlyingBuffer().getMaxAllocatedBytes());
+    EXPECT_EQ(1, a.getUnderlyingBuffer().getAllocationCount());
+    EXPECT_EQ(2, a.getUnderlyingBuffer().getMaxAllocationCount());
+
+    a.deallocate(ptr2, 5);
+    EXPECT_EQ(0, a.getUnderlyingBuffer().getAllocatedBytes());
+    EXPECT_EQ(20 * sizeof(int), a.getUnderlyingBuffer().getMaxAllocatedBytes());
+    EXPECT_EQ(0, a.getUnderlyingBuffer().getAllocationCount());
+    EXPECT_EQ(2, a.getUnderlyingBuffer().getMaxAllocationCount());
+}
+
+/// Check that there is an allocation limit
+TEST(ThreadLocalMonotonicResource, Limit) {
+    struct Tag {};
+    using Resource = cb::ThreadLocalMonotonicResource<Tag,
+                                                      10 * sizeof(int),
+                                                      20 * sizeof(int)>;
+    using Allocator = Resource::Allocator<int>;
+    Allocator a;
+
+    a.allocate(10);
+    EXPECT_THROW(a.allocate(11), std::bad_alloc);
+    EXPECT_NO_THROW(a.allocate(10));
+}
+
+/// Check that the underlying buffer is thread-local.
+TEST(ThreadLocalMonotonicResource, ThreadLocal) {
+    struct Tag {};
+    using Resource = cb::ThreadLocalMonotonicResource<Tag, 0, 0>;
+    using Allocator = Resource::Allocator<int>;
+
+    Allocator a;
+    auto& buffer = a.getUnderlyingBuffer();
+
+    std::thread t(
+            [&a, &buffer]() { EXPECT_NE(&buffer, &a.getUnderlyingBuffer()); });
+    t.join();
+}
+
+/// Check that the allocations are not made under any arena.
+TEST(ThreadLocalMonotonicResource, NoArenaGuard) {
+    struct Tag {};
+    using Resource = cb::ThreadLocalMonotonicResource<Tag, 100, 200>;
+
+    ASSERT_TRUE(cb_malloc_is_using_arenas());
+
+    auto client = cb::ArenaMalloc::registerClient(false);
+
+    std::thread t([&client]() {
+        cb::ArenaMalloc::switchToClient(client);
+        auto& buffer = Resource::getThreadBuffer();
+        const size_t initialAllocated =
+                cb::ArenaMalloc::getPreciseAllocated(client);
+        {
+            // GTest lazily initialises heap structures.
+            cb::NoArenaGuard gtest;
+            EXPECT_EQ(initialAllocated,
+                      cb::ArenaMalloc::getPreciseAllocated(client));
+        }
+        buffer.allocate(100);
+        {
+            cb::NoArenaGuard gtest;
+            EXPECT_EQ(initialAllocated,
+                      cb::ArenaMalloc::getPreciseAllocated(client));
+        }
+        buffer.allocate(100);
+        {
+            cb::NoArenaGuard gtest;
+            EXPECT_EQ(initialAllocated,
+                      cb::ArenaMalloc::getPreciseAllocated(client));
+        }
+        cb::ArenaMalloc::switchFromClient();
+    });
+
+    t.join();
+    cb::ArenaMalloc::unregisterClient(client);
+}
